c848fe85b42a1832ab84258f68e71cbb813f243c
shizopudge <pika.down@yandex.ru>
2023-08-01 23:16:30 +0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './core/service/prisma.service';
-import { initializeApp, applicationDefault} from 'firebase-admin/app';
+import { initializeApp, applicationDefault } from 'firebase-admin/app';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000
-  const app = await NestFactory.create(AppModule)
-  const prismaService = app.get(PrismaService)
-  await prismaService.enableShutdownHooks(app)
-  app.setGlobalPrefix('api')
-  app.enableCors()
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   initializeApp({
     credential: applicationDefault(),
   });
-  await app.listen(PORT)
+  await app.listen(PORT);
 }
 bootstrap();
