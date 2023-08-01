bbd8d8d8d9e184ec0a6ee1f889e2907a5414d8d6
shizopudge <pika.down@yandex.ru>
2023-08-02 02:47:52 +0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { AppModule } from './app.module';
 import { PrismaService } from './core/service/prisma.service';
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
 import * as fs from 'fs';
+import cors from 'cors';
 
 async function bootstrap() {
   const privateKey = fs.readFileSync('./src/server.key', 'utf8');
   const certificate = fs.readFileSync('./src/server.cert', 'utf8');
   const httpsOptions = { key: privateKey, cert: certificate };
-  console.log(httpsOptions);
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { httpsOptions });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   app.setGlobalPrefix('api');
+  app.use(cors());
   initializeApp({
     credential: applicationDefault(),
   });
